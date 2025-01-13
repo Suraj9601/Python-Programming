7b0e8ce40347e018141fe076fa15c3dda1cd0610
Suraj Aglave <aglavesuraj96@gmail.com>
2025-01-13 15:25:27 +0530

    Initial commit

---
+++ Input.py
@@ -0,0 +1,8 @@
+
+name= input("Name :")
+village = input("Village : ")
+age = int(input("Age :"))
+
+print("My name is",name)
+print("I am from",village)
+print("I am",age, "year old")
---
+++ Traffic lights.py
@@ -0,0 +1,9 @@
+light = input("light color ")
+if(light == "Red" or light == "red"):
+    print("Stop")
+elif(light == "Green" or light == "green"):
+    print("Go")
+elif(light == "Yellow" or light == "yellow"):
+    print("Look")
+else:
+    print("Light is Broken.")
