631d3421bd8cbd2ecc43f598255da24b88bdf197
Suraj Aglave <aglavesuraj96@gmail.com>
2025-01-13 15:53:58 +0530

    Create Simple Interest.py

---
+++ Simple Interest.py
@@ -0,0 +1,8 @@
+# Input of p,r,t from user
+p = int(input("p :"))
+r = int(input("r :"))
+t = int(input("t :"))
+# Calculating the simple interest
+si = (p * r * t) / 100
+# Printing the result
+print("Simple Interest is : ", si)
