9a8a3c9a1ebade2b49f753583ee91da773366bd9
Suraj Aglave <aglavesuraj96@gmail.com>
2025-01-13 15:54:14 +0530

    Create Vote Eligibility.py

---
+++ Vote Eligibility.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age :"))
+
+if(age >= 18):
+    print("Congratulation")
+    print("You are Eligible for Vote.")
+elif(age < 18):
+    print("Sorry")
+    print("You are not Eligible for Vote.")
+
