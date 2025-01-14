4f5b7c412fcee59ec51975c7b94ceb454f1356c9
Suraj Aglave <aglavesuraj96@gmail.com>
2025-01-14 19:32:39 +0530

    Create Operator.py

---
+++ Operator.py
@@ -0,0 +1,19 @@
+a = 20
+b = 10
+
+# Arithmetic Operator
+print("Arithmetic Operator")
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+
+# Relational Operator
+print("Relational Operator")
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
