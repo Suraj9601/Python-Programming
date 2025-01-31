c59c128c4290a11dc858e371e6bdb48aa5cf019f
Suraj Aglave <aglavesuraj96@gmail.com>
2025-01-31 15:32:09 +0530

    Create Practice Que 4.py

---
+++ Practice Que 4.py
@@ -0,0 +1,10 @@
+'''Write a program to ask the user to enter names of their 
+3 favorite movies and store them in list''' 
+
+list =[]
+
+list.append(input("Enter the first movie name :"))
+list.append(input("Enter the second movie name :"))
+list.append(input("Enter the third movie name :"))
+
+print(list)
