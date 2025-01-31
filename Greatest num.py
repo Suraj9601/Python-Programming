e593d4c6416f8d1ed8692c7ccb42a5c14f54febf
Suraj Aglave <aglavesuraj96@gmail.com>
2025-01-31 15:32:17 +0530

    Update Greatest num.py

--- Greatest num.py
+++ Greatest num.py
@@ -1,7 +1,14 @@
 num1 = int(input("Enter first number :"))
 num2 = int(input("Enter second number :"))
+num3 = int(input("Enter third number :"))
 
 if(num1 > num2):
-    print(num1,"number is greater than",num2)
+    if(num1 > num3):
+        print(num1,"is the greatest number")
+    else:
+        print(num3,"is the greatest number.")
 else:
-    print(num2,"number is greater than",num1)
+    if(num2 > num3):
+        print(num2,"is the greatest number")
+    else:
+        print(num3,"is the greatest number.")
