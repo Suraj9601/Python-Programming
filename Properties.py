51ffa5563e22f1632a73dbf1a051c545be25a612
Suraj Aglave <aglavesuraj96@gmail.com>
2025-01-31 15:32:02 +0530

    Create Properties.py

---
+++ Properties.py
@@ -0,0 +1,11 @@
+# Indexing
+str = "suraj Aglave"
+ch = str[0] #ch = characters in string
+print(ch)
+
+# Slicing
+print(str[0:5]) # str[0:5] = str[:5]
+print(str[6:13]) # str[6:13] = str[6:] = str[6:len(str)]
+
+# Slicing negative index
+print(str[-10:6])
