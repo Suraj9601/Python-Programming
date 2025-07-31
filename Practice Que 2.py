4a298408d6e0ad4f509d829800ec9fc8e3aad014
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-31 17:12:14 +0530

    Delete Practice Que 2.py

--- Practice Que 2.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Enter the first number :"))
-num2 = int(input("Enter the second number :"))
-
-# if(num1>=num2):
-#     print("True")
-# else:
-#     print("False")
-
-print(num1>=num2)
