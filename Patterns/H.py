866f555ff23169af04de287fb80b8477a670b618
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 00:03:52 +0530

    Create H.py

---
+++ Patterns/H.py
@@ -0,0 +1,19 @@
+def pat():
+    for i in range(1, 8):         # rows
+        for j in range(1, 8):     # cols
+            
+            if i == 4 :
+                print("*", end=" ")
+
+            
+            elif j == 1:
+                print("*", end=" ")
+
+            elif j == 7:
+                print("*", end=" ")
+
+            else:
+                print(" ", end=" ")
+        print()
+
+pat()
