db26407569f5547e23eed46a2d81e648883e169a
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 00:18:10 +0530

    Update C.py

--- Patterns/Characters/C.py
+++ Patterns/Characters/C.py
@@ -1,8 +1,19 @@
+'''
+      # # # # 
+    #
+    #
+    #
+      # # # #
+'''
+
 def pat():
-    for i in range(1,6):
-        for j in range(1,6):
-            if i == 1 or i == 5 or j == 1:
-                print("*", end=" ")
+    n = 5
+    for i in range(1, n+1):          # rows
+        for j in range(1, n+1):      # cols
+            if (i == 1 and j > 1) or (i == n and j > 1) or (j == 1 and i != 1 and i != n):
+                print("#", end=" ")
+            else:
+                print(" ", end=" ")
         print()
-            
+
 pat()
