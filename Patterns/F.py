7fcb50cc72cb920ab01da4855ff310054924590e
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 00:01:41 +0530

    Create F.py

---
+++ Patterns/F.py
@@ -0,0 +1,10 @@
+def pat():
+    for i in range(1, 8):         # rows
+        for j in range(1, 8):     # cols
+            if i == 1 or i == 4 or j == 1:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+pat()
