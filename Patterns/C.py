012b8940b14caaffa4a3d8b968ade2be5f608982
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 00:05:55 +0530

    Delete C.py

--- Patterns/C.py
+++
@@ -1,8 +0,0 @@
-def pat():
-    for i in range(1,6):
-        for j in range(1,6):
-            if i == 1 or i == 5 or j == 1:
-                print("*", end=" ")
-        print()
-            
-pat()
