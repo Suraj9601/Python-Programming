0654469f3a44c56f280466b49dc75f655e43c34a
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 00:09:39 +0530

    Update G.py

--- Patterns/Characters/G.py
+++ Patterns/Characters/G.py
@@ -1,21 +1,32 @@
+'''
+# # # # # # # 
+#
+#
+#     # # # #
+#           #
+#           #
+# # # # # # # 
+
+'''
+
 def pat():
     for i in range(1, 8):         # rows
         for j in range(1, 8):     # cols
             
             if i == 1  :
-                print("*", end=" ")
+                print("#", end=" ")
 
             elif  i == 7:
-                print("*", end=" ")
+                print("#", end=" ")
 
             elif j == 1:
-                print("*", end=" ")
+                print("#", end=" ")
 
             elif  (i >= 4 and j == 7):
-                print("*", end=" ")
+                print("#", end=" ")
 
             elif (i == 4 and j >= 4):
-                print("*", end=" ")
+                print("#", end=" ")
 
             else:
                 print(" ", end=" ")
