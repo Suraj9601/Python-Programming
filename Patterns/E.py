f2d987ad9ba80be5b0685abaf264b7b669eec981
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-03 23:47:38 +0530

    Create E.py

---
+++ Patterns/E.py
@@ -0,0 +1,11 @@
+def pat():
+    print()
+    for i in range(1, 8):         # rows
+        for j in range(1, 8):     # cols
+            if i == 1 or i == 4 or i == 7 or j == 1:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+pat()
