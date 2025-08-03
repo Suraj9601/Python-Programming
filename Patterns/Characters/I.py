125e011a959231938875ffac8ff7b3dc59ad63f7
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 00:51:39 +0530

    Update I.py

--- Patterns/Characters/I.py
+++ Patterns/Characters/I.py
@@ -1,15 +1,26 @@
+'''
+# # # # # # # 
+      #
+      #
+      #
+      #
+      #
+# # # # # # #
+
+'''
+
 def pat():
     for i in range(1, 8):         # rows
         for j in range(1, 8):     # cols
             
-            if i == 4 :
+            if i == 1 :
                 print("#", end=" ")
 
             
-            elif j == 1:
+            elif i == 7:
                 print("#", end=" ")
 
-            elif j == 7:
+            elif j == 4:
                 print("#", end=" ")
 
             else:
