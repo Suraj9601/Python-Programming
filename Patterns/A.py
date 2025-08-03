6eb2408c59e71fa862ec10f98bf0b38660da795a
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-03 23:47:41 +0530

    Update A.py

--- Patterns/A.py
+++ Patterns/A.py
@@ -1,12 +1,11 @@
-# Print 'A' Pattern in Python
+def pat():
+    for i in range(1,6): # row
+        for j in range(1,6): # column
+            if i == 1 or j == 1 or i == 3 or j == 5:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+    return
 
-n = 7   # height of A
-
-for i in range(n):
-    for j in range(n):
-        # Top line, middle line, left & right slants
-        if ((j == 0 or j == n-1) and i != 0) or (i == 0 and j > 0 and j < n-1) or (i == n//2):
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
-    print()
+pat()
