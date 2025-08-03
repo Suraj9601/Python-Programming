910820a8b4facddf8dda4ec6ffa4beff20893175
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-03 23:47:27 +0530

    Create D.py

---
+++ Patterns/D.py
@@ -0,0 +1,10 @@
+def pat():
+    for i in range(1, 8):         # rows
+        for j in range(1, 8):     # cols
+            if i == 1 or i == 7 or j == 2 or j == 7:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+pat()
