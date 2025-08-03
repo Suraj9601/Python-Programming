0634b696f4bc889df42d4283a00c315f4bcb7fa0
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 00:51:36 +0530

    Update F.py

--- Patterns/Characters/F.py
+++ Patterns/Characters/F.py
@@ -1,8 +1,19 @@
+'''
+# # # # # # # 
+#
+#
+# # # # # # #
+#
+#
+#
+
+'''
+
 def pat():
     for i in range(1, 8):         # rows
         for j in range(1, 8):     # cols
             if i == 1 or i == 4 or j == 1:
-                print("*", end=" ")
+                print("#", end=" ")
             else:
                 print(" ", end=" ")
         print()
