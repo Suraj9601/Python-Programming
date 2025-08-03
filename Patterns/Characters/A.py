90e731ee764f3c66ca1f3ad13c01556e7ec1f17c
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 00:10:57 +0530

    Update A.py

--- Patterns/Characters/A.py
+++ Patterns/Characters/A.py
@@ -1,8 +1,19 @@
+'''
+# # # # # # # 
+#           #
+#           #
+# # # # # # #
+#           #
+#           #
+#           # 
+
+'''
+
 def pat():
-    for i in range(1,6): # row
-        for j in range(1,6): # column
-            if i == 1 or j == 1 or i == 3 or j == 5:
-                print("*", end=" ")
+    for i in range(1,8): # row
+        for j in range(1,8): # column
+            if i == 1 or j == 1 or i == 4 or j == 7:
+                print("#", end=" ")
             else:
                 print(" ", end=" ")
         print()
