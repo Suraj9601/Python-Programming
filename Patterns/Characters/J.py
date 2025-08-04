ec846756c722482b736901a64b28c79561b5c2f3
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 18:56:14 +0530

    Create J.py

---
+++ Patterns/Characters/J.py
@@ -0,0 +1,35 @@
+'''
+    # # # # # # # 
+            #
+            #
+            #
+    #       #
+    #       #
+    # # # # # 
+
+'''
+
+
+def pat():
+    for i in range(1, 8):         # rows
+        for j in range(1, 8):     # cols
+            
+                
+            if i == 1:
+                print("#", end=" ")
+
+            
+            elif i == 7 and j <= 4:
+                print("#", end=" ")
+
+            elif i>= 5 and j == 1:
+                print("#", end=" ")
+                
+            elif j == 5:
+                print("#", end=" ")
+
+            else:
+                print(" ", end=" ")
+        print()
+
+pat()
