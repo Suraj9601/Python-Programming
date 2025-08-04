4ae2daa61ef9baf59e63a50ce4670d0416b3ee09
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-04 21:56:03 +0530

    Update O.py

--- Patterns/Characters/O.py
+++ Patterns/Characters/O.py
@@ -10,16 +10,25 @@
 '''
 
 def pat():
-    for i in range(8):      # rows: 0 to 7
-        for j in range(8):  # cols: 0 to 7
-            # Top and bottom curves
-            if (i == 0 or i == 7) and 2 <= j <= 5:
+    for i in range(1, 8):         # rows
+        for j in range(1, 10):    # cols
+            # Top curve
+            if i == 1 and 3 <= j <= 7:
                 print("#", end=" ")
-            # Upper and lower sides
-            elif (i == 1 or i == 6) and (j == 1 or j == 6):
+            # Bottom curve
+            elif i == 7 and 3 <= j <= 7:
                 print("#", end=" ")
-            # Left and right verticals
-            elif 2 <= i <= 5 and (j == 0 or j == 7):
+            # Upper left curve
+            elif i == 2 and (j == 2 or j == 8):
+                print("#", end=" ")
+            # Lower left curve
+            elif i == 6 and (j == 2 or j == 8):
+                print("#", end=" ")
+            # Left vertical
+            elif 3 <= i <= 5 and j == 1:
+                print("#", end=" ")
+            # Right vertical
+            elif 3 <= i <= 5 and j == 9:
                 print("#", end=" ")
             else:
                 print(" ", end=" ")
