e69c8c5dac4cb918fa99fde1ba35f3b761ef5f1d
Suraj Aglave <aglavesuraj96@gmail.com>
2025-09-27 12:21:34 +0530

    Create List_Indexing&Slicing.py

---
+++ List_Array/List_Indexing&Slicing.py
@@ -0,0 +1,9 @@
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers[0])   # 10 (first element)
+print(numbers[-1])  # 50 (last element)
+
+# Slicing
+print(numbers[1:4]) # print element of 1,2,3
+print(numbers[:3])  # print element of 0,1,2
+print(numbers[::2]) # [10, 30, 50] (step 2)
