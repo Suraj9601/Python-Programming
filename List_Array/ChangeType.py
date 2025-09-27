802646cb9a524619a6284ebf6293f30b767bb5da
Suraj Aglave <aglavesuraj96@gmail.com>
2025-09-27 12:21:20 +0530

    Update ChangeType.py

--- List_Array/ChangeType.py
+++ List_Array/ChangeType.py
@@ -1,6 +1,7 @@
-list = [1,2,3,4,5,6,7]
-print(type(list[0]))
+list = [1, 2, 3, 4, 5, 6, 7]
+print("Original List type : ", type(list[0]))
 print(list)
 
 new_arr = [float(i) for i in list]
+print("New List type : ", type(new_arr[0]))
 print(new_arr)
