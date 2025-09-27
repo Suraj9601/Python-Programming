6f1fcb413298ed16705b0ac2daf74b672a4fb96a
Suraj Aglave <aglavesuraj96@gmail.com>
2025-09-27 12:33:30 +0530

    Create List_Function.py

---
+++ List_Array/List_Function.py
@@ -0,0 +1,6 @@
+nums = [1, 2, 3, 4, 5]
+
+print("Length of List : ",len(nums))       
+print("Smallest number in List : ",min(nums))       
+print("Largest number in List : ",max(nums))       
+print("Sum of List element : ",sum(nums))       
