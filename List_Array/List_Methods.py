241ff513d0600750462cd21a8a18aab000f01cd2
Suraj Aglave <aglavesuraj96@gmail.com>
2025-09-27 12:21:31 +0530

    Create List_Methods.py

---
+++ List_Array/List_Methods.py
@@ -0,0 +1,11 @@
+nums = [5, 2, 9, 1]
+
+nums.append(7)          # add at end
+nums.insert(1, 100)     # add at index
+nums.remove(2)          # remove value
+nums.pop()              # remove last
+nums.sort()             # ascending sort
+nums.reverse()          # reverse list
+nums.clear()            # empty list
+
+print(nums)
