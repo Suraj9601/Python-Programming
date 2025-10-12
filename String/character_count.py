65e4086511e89cb7cef77a34ec746ccc2cddbcd4
Suraj Aglave <aglavesuraj96@gmail.com>
2025-10-12 15:29:20 +0530

    Update character_count.py

--- String/character_count.py
+++ String/character_count.py
@@ -1,12 +1,15 @@
+def character_count(str):
+    print(f"Character Count from string : '{str}'")
+    char_count = {}
+
+    for ch in str:
+        if ch in char_count:
+            char_count[ch] += 1
+        else:
+            char_count[ch] = 1
+    return char_count
+
+
 str = "banana"
-print(f"Character Count from string : '{str}'")
-
-char_count = {}
-
-for ch in str:
-    if ch in char_count:
-        char_count[ch] += 1
-    else:
-        char_count[ch] = 1
-
-print(char_count)
+result = character_count(str)
+print(result)
