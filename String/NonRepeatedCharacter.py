a30fbbb85f1c9de4bd0551ac6e13023493c5ffdf
Suraj Aglave <aglavesuraj96@gmail.com>
2025-10-18 20:05:54 +0530

    Update NonRepeatedCharacter.py

--- String/NonRepeatedCharacter.py
+++ String/NonRepeatedCharacter.py
@@ -2,10 +2,6 @@ def first_non_repeated_char(str):
     count = {}
     for ch in str:
         count[ch] = count.get(ch,0) + 1
-        
-    
-    
-    
 
 str = "aabbcde"
 print(first_non_repeated_char(str))  
