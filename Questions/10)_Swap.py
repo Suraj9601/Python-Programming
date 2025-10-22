95a5b250d452c3e3aaac0d15c9bd17622b8bbc2a
Suraj Aglave <aglavesuraj96@gmail.com>
2025-10-22 20:46:44 +0530

    Create 10)_Swap.py

---
+++ Questions/10)_Swap.py
@@ -0,0 +1,10 @@
+n1 = int(input("Enter the first number : "))
+n2 = int(input("Enter the second number : "))
+
+print("Before swapping : ")
+print(f"n1 : {n1} & n2 : {n2}")
+
+n1 , n2 = n2 , n1
+
+print("After swapping : ")
+print(f"n1 : {n1} & n2 : {n2}")
